c497eca0256eb6be6120cf3b285cd97ef3032b4a
Som64 <soja19@student.bth.se>
2024-05-17 10:05:33 +0200

    Ljus senaste
    Projektet gick sönder nästan

--- Somchai/2DCandle.gd
+++ Somchai/2DCandle.gd
@@ -3,15 +3,22 @@ extends Node2D
 var light_sprite
 var light_pivot
 var candle_sprite
+var candle_number
 
 var roll = 0.0
 
+var target_scale = Vector2(0.5, 0.5)  # Initial or default scale
+var smooth_rate = 0.8  # Adjust for desired smoothing speed (lower for slower)
+
+
 func _ready():
-	light_sprite = get_node("Pivot/Light")
-	light_pivot = get_node("Pivot")
-	candle_sprite = get_node(("Sprite2D"))
+	light_sprite = get_node("Container/Pivot/Light")
+	light_pivot = get_node("Container/Pivot")
+	candle_sprite = get_node(("Container/Sprite2D"))
+	candle_number = get_node(("Number"))
 	#light_sprite.scale = Vector2(3, 1)
 	light_sprite.play()
+	candle_number.visible = false
 	
 func _process(delta):
 	var gyro = Input.get_gyroscope()
@@ -27,5 +34,22 @@ func _process(delta):
 	var gyroscope: Vector3 = Input.get_gyroscope().rotated(-Vector3.FORWARD, roll)
 	roll = lerp_angle(roll_acc, roll + gyroscope.z * delta, 0.98)
 	
-	print(str(roll))
+	light_pivot.scale = light_pivot.scale.lerp(target_scale, smooth_rate * delta)
+	
+	print("roll:" + str(roll))
+	if(roll > -3 && roll < 0):
+		print(str("hot"))
+		Input.vibrate_handheld(100)
+		target_scale = Vector2(1,1)
+		candle_number.visible = true
+		
+	else:
+		target_scale = Vector2(0.5, 0.5)
+		
 	light_pivot.rotation = roll
+	
+	
+#3-
+#0
+
+#-2--1
